d0e5d5340027af2577ad8c882ceca43224059be9
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-20 16:32:53 +0700

    Add coupon seeder

---
+++ database/seeds/CouponSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Eloquent\CounselorAccount;
+use App\Eloquent\Coupon;
+use App\Generators\CouponGenerator;
+use Illuminate\Database\Seeder;
+
+/**
+ * This <konseling-1> project created by :
+ * Name         : syafiq
+ * Date / Time  : 19 November 2017, 5:21 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+class CouponSeeder extends Seeder
+{
+    use CouponGenerator;
+
+    public function run()
+    {
+        $nip = '125150200111112';
+        /** @var \Illuminate\Database\Query\Builder $model */
+        $model = new CounselorAccount;
+        /** @var CounselorAccount $admin */
+        if ($admin = $model->where('credential', '=', $nip)->first())
+        {
+            $count = 6;
+            while (--$count)
+            {
+                while (1)
+                {
+                    try
+                    {
+                        $coupon = new Coupon(['coupon' => $this->generate()]);
+                        $admin->coupon()->save($coupon);
+                        break;
+                    }
+                    catch (Illuminate\Database\QueryException $ignored)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
+
+?>
