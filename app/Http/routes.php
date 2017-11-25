bb0f67f6c15e3d52581dd70a2657fab6c8c27896
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-25 18:11:16 +0700

    Add proper guest middleware to root and auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,7 @@
 |
 */
 /** @var \Illuminate\Routing\Router $router */
-$router->get('/', ['as' => 'root', 'uses' => 'WelcomeController@index']);
+$router->get('/', ['middleware' => 'guest', 'as' => 'root', 'uses' => 'WelcomeController@index']);
 $router->group(['prefix' => '/template', 'middleware' => 'debug'], function () use ($router) {
     $router->get('/boilerplate', 'TemplateController@boilerplate');
     $router->get('/bootstrap', 'TemplateController@bootstrap');
@@ -19,9 +19,9 @@ $router->group(['prefix' => '/template', 'middleware' => 'debug'], function () u
 });
 $router->group(['namespace' => 'Counselor', 'prefix' => '/counselor'], function () use ($router) {
     $router->group(['prefix' => '/auth'], function () use ($router) {
-        //@formatter:off
-        $router->get(   '/register', ['uses' => 'Auth@registerCreate', 'as' => 'counselor.auth.register.create']);
-        $router->post(  '/register', ['middleware' => 'auth.role.counselor', 'uses' => 'Auth@registerStore' , 'as' => 'counselor.auth.register.store']);
-        //@formatter:on
+        $router->group(['middleware' => 'guest'], function () use ($router) {
+            $router->get('/register', ['uses' => 'Auth@registerCreate', 'as' => 'counselor.auth.register.create']);
+            $router->post('/register', ['middleware' => 'auth.role.counselor', 'uses' => 'Auth@registerStore', 'as' => 'counselor.auth.register.store']);
+        });
     });
 });
