77c56ce238e2c085d53a1234f05a7b5b04c2ca3b
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-25 11:53:08 +0700

    Update counselor account model

--- app/Eloquent/CounselorAccount.php
+++ app/Eloquent/CounselorAccount.php
@@ -1,9 +1,12 @@
 <?php namespace App\Eloquent;
 
+use App\Generators\DefaultAvatarGenerator;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class CounselorAccount extends Model
+class CounselorAccount extends Model implements Authenticatable
 {
+    use DefaultAvatarGenerator;
     /**
      * @var bool
      */
@@ -71,4 +74,63 @@ class CounselorAccount extends Model
         return 'Y-m-d H:i:s';
     }
 
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->getAttribute('password');
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return $this->getAttribute($this->getRememberTokenName());
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->setAttribute($this->getRememberTokenName(), $value);
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+
+    /** Get User Role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return 'counselor';
+    }
 }
