fa1985c2d2e29606ea90687d702f54039e48216c
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-25 11:54:28 +0700

    Add register

--- app/Http/Controllers/Counselor/Auth.php
+++ app/Http/Controllers/Counselor/Auth.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers\Counselor;
 
 use App\Http\Controllers\Controller;
+use App\Services\CounselorRegistrar;
 use Illuminate\Http\Request;
 
 class Auth extends Controller
@@ -16,9 +17,33 @@ class Auth extends Controller
         return view("layout.counselor.auth.register.counselor_auth_register_$this->theme");
     }
 
-    public function registerStore(Request $request)
+    public function registerStore(CounselorRegistrar $registrar, Request $request)
     {
+        $validator = $registrar->validator($request->all());
 
+        if ($validator->fails())
+        {
+            $this->throwValidationException(
+                $request, $validator
+            );
+        }
+
+        $user = $registrar->create($request->all());
+        Log::debug($user);
+
+        return redirect($this->redirectPath());
+    }
+
+    public function redirectPath()
+    {
+        Log::debug($this->redirectPath);
+        Log::debug($this->redirectTo);
+        if (property_exists($this, 'redirectPath'))
+        {
+            return $this->redirectPath;
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/counselor';
     }
 
 }
