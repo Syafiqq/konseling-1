485d318c3fb4745069b03af4017f2a5a7479a97b
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-25 11:52:53 +0700

    Add counselor registrar

---
+++ app/Services/CounselorRegistrar.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This <konseling-1> project created by :
+ * Name         : syafiq
+ * Date / Time  : 20 November 2017, 6:44 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Services;
+
+
+use App\Eloquent\CounselorAccount;
+use Illuminate\Contracts\Auth\Registrar;
+use Illuminate\Support\Facades\Validator;
+
+class CounselorRegistrar implements Registrar
+{
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public function validator(array $data)
+    {
+        /** @var \Illuminate\Validation\Factory $validator */
+        $validator = Validator::getFacadeRoot();
+
+        return $validator->make($data, [
+            'credential' => 'required|max:100|unique:counselor_accounts',
+            'name' => 'required|max:100',
+            'gender' => 'required|in:male,female',
+            'password' => 'required|confirmed|min:8',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array $data
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public function create(array $data)
+    {
+        /** @var \Illuminate\Database\Query\Builder $model */
+        $model = new CounselorAccount;
+        $model->setAttribute('credential', $data['credential']);
+        $model->setAttribute('name', $data['name']);
+        $model->setAttribute('gender', $data['gender']);
+        $model->setAttribute('avatar', $model->generate($model->getAttribute('gender')));
+        $model->setAttribute('password', bcrypt($data['password']));
+
+        //$model->save();
+
+        return $model;
+    }
+}
+
+?>
