5193309ca40de24860d9b461131eae5786faaa30
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-26 13:13:35 +0700

    Fix Student controller

--- app/Http/Controllers/Student/Auth.php
+++ app/Http/Controllers/Student/Auth.php
@@ -1,12 +1,20 @@
 <?php namespace App\Http\Controllers\Student;
 
+use App\Contracts\RedirectResolver;
+use App\Contracts\Roleable;
 use App\Http\Controllers\AuthFlow;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\PathRedirector;
+use App\Services\StudentRegistrar;
+use Illuminate\Auth\Guard;
+use Illuminate\Http\Request;
 
-class Auth extends Controller
+class Auth extends Controller implements Roleable, RedirectResolver
 {
-    use PathRedirector, AuthFlow;
+    use PathRedirector, AuthFlow
+    {
+        registerStore as private _registerStore;
+    }
 
     public function getLogin()
     {
@@ -18,8 +26,22 @@ class Auth extends Controller
         return view("layout.student.auth.register.student_auth_register_$this->theme");
     }
 
+    public function registerStore(StudentRegistrar $registrar, Request $request, Guard $guard)
+    {
+        return $this->_registerStore($registrar, $request, $guard);
+    }
+
+
     public function redirectPath()
     {
         return $this->redirect('/student/dashboard');
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole()
+    {
+        return 'student';
     }
 }
