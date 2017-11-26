e1de4d2ac32d68fa40f6f9bb668e47d3a0442262
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-26 13:15:18 +0700

    Deactivate saving method

--- app/Services/UserRegistrar.php
+++ app/Services/UserRegistrar.php
@@ -54,7 +54,7 @@ abstract class UserRegistrar implements Registrar, Roleable
         $model->setAttribute('avatar', $model->generate($model->getAttribute('gender')));
         $model->setAttribute('password', bcrypt($data['password']));
 
-        $model->save();
+        //$model->save();
 
         return $model;
     }
