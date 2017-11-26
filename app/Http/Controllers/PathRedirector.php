cb00f3d97c9dd36fd4b07de26bdee2cfe367024e
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-26 09:35:19 +0700

    Add Path Redirector Trait

---
+++ app/Http/Controllers/PathRedirector.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This <konseling-1> project created by :
+ * Name         : syafiq
+ * Date / Time  : 26 November 2017, 12:07 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace App\Http\Controllers;
+
+
+trait PathRedirector
+{
+    /**
+     * @param string $path
+     * @return mixed
+     */
+    private function redirect($path)
+    {
+        if (property_exists($this, 'redirectPath'))
+        {
+            return $this->redirectPath;
+        }
+
+        return property_exists($this, 'redirectTo') ? $this->redirectTo : $path;
+    }
+}
+
+?>
