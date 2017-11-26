e9731170b4850c284bac57d80bab09cbca0972d0
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-26 13:11:28 +0700

    Fix Counselor Controller

--- app/Http/Controllers/Counselor/Auth.php
+++ app/Http/Controllers/Counselor/Auth.php
@@ -1,12 +1,20 @@
 <?php namespace App\Http\Controllers\Counselor;
 
+use App\Contracts\RedirectResolver;
+use App\Contracts\Roleable;
 use App\Http\Controllers\AuthFlow;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\PathRedirector;
+use App\Services\CounselorRegistrar;
+use Illuminate\Auth\Guard;
+use Illuminate\Http\Request;
 
-class Auth extends Controller
+class Auth extends Controller implements Roleable, RedirectResolver
 {
-    use PathRedirector, AuthFlow;
+    use PathRedirector, AuthFlow
+    {
+        registerStore as private _registerStore;
+    }
 
     public function getLogin()
     {
@@ -18,8 +26,18 @@ class Auth extends Controller
         return view("layout.counselor.auth.register.counselor_auth_register_$this->theme");
     }
 
+    public function registerStore(CounselorRegistrar $registrar, Request $request, Guard $guard)
+    {
+        return $this->_registerStore($registrar, $request, $guard);
+    }
+
     public function redirectPath()
     {
         return $this->redirect('/counselor/dashboard');
+    }
+
+    public function getRole()
+    {
+        return 'counselor';
     }
 }
