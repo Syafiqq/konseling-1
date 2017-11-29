695b3a82ceab659a2a21ed185147279b323efecd
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-29 21:01:51 +0700

    Attach Coupon Generator

--- app/Http/Controllers/Counselor/Home.php
+++ app/Http/Controllers/Counselor/Home.php
@@ -1,12 +1,41 @@
 <?php namespace App\Http\Controllers\Counselor;
 
+use App\Eloquent\Coupon;
+use App\Eloquent\User;
+use App\Generators\CouponGenerator;
 use App\Http\Controllers\Controller;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Session;
 
 class Home extends Controller
 {
+    use CouponGenerator;
 
     public function index()
     {
+        /** @noinspection PhpUndefinedMethodInspection */
+        dd(Session::get('cbk_msg', ''));
     }
 
+    public function couponGenerator()
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        /** @var User $user */
+        $user = \Illuminate\Support\Facades\Auth::user();
+        while (1)
+        {
+            try
+            {
+                $coupon = new Coupon(['coupon' => $this->generate()]);
+                $user->coupon()->save($coupon);
+                break;
+            }
+            catch (QueryException $ignored)
+            {
+            }
+        }
+        $redirect = redirect()->intended(route('counselor.home.dashboard'))->with('cbk_msg', 'Kupon Berhasil Ditambahkan');
+
+        return $redirect;
+    }
 }
