54abb8311c8e1eabd194728a870f7edb20f31122
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-01 15:51:40 +0700

    Register Question seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         $this->call('QuestionCategoriesSeeder');
         $this->call('QuestionFavoursSeeder');
         $this->call('QuestionOptionsSeeder');
+        $this->call('QuestionsSeeder');
     }
 
 }
