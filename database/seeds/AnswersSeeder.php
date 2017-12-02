662f2e6e8d8e385f8f8948c3efabb9034a8e01a3
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-02 22:25:11 +0700

    Distribute answer to new user

--- database/seeds/AnswersSeeder.php
+++ database/seeds/AnswersSeeder.php
@@ -15,17 +15,17 @@ class AnswersSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['user' => '2', 'started_at' => '2017-11-09 07:00:00', 'finished_at' => '2017-11-09 08:00:00'],
-            ['user' => '2', 'started_at' => '2017-11-16 07:00:00', 'finished_at' => '2017-11-16 08:00:00'],
-            ['user' => '2', 'started_at' => '2017-11-22 07:00:00', 'finished_at' => '2017-11-22 08:00:00'],
             ['user' => '2', 'started_at' => '2017-11-30 07:00:00'],
+            ['user' => '3', 'started_at' => '2017-11-30 07:00:00', 'finished_at' => '2017-11-30 08:00:00'],
+            ['user' => '4', 'started_at' => '2017-11-26 07:00:00', 'finished_at' => '2017-11-26 08:00:00'],
+            ['user' => '4', 'started_at' => '2017-11-30 07:00:00', 'finished_at' => '2017-11-30 08:00:00'],
         ];
 
         /** @var \Illuminate\Database\Query\Builder $model */
         $model = new Answer();
         foreach ($data as $category)
         {
-            if (!$model->where('started_at', '=', $category['started_at'])->first())
+            if (!$model->where('started_at', '=', $category['started_at'])->where('user', '=', $category['user'])->first())
             {
                 $model->insert($category);
             }
