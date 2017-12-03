112a5f76e1307127692f1c16b3a01ec16aedb22f
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-03 08:38:04 +0700

    Add Student Activation

--- app/Http/Controllers/Counselor/Report.php
+++ app/Http/Controllers/Counselor/Report.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers\Counselor;
 
 use App\Eloquent\User;
+use App\Eloquent\UserStudents;
 use App\Http\Controllers\Controller;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Session;
@@ -17,5 +18,15 @@ class Report extends Controller
         }])->where('role', '=', 'student')->get();
 
         return view("layout.counselor.report.list.counselor_report_list_$this->theme", ['reports' => $reports]);
+    }
+
+    public function activation($id)
+    {
+        /** @var UserStudents $student */
+        $student = UserStudents::where('user', '=', $id)->first();
+        $student->setAttribute('active', true);
+        $student->save();
+
+        return redirect()->back()->with('cbk_msg', ['notify' => ['Siswa Berhasil Diaktifkan']]);
     }
 }
