db8957b9ebfc614f8b0367928e25aeb2f12a1152
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-05 05:09:15 +0700

    Add Redirect to CourseSession Creation Add CourseStart function

--- app/Http/Controllers/Student/Course.php
+++ app/Http/Controllers/Student/Course.php
@@ -4,16 +4,21 @@ use App\Eloquent\Answer;
 use App\Eloquent\AnswerDetail;
 use App\Eloquent\AnswerResult;
 use App\Eloquent\Question;
+use App\Eloquent\QuestionOption;
 use App\Eloquent\User;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class Course extends Controller
 {
 
     public function index()
     {
+        var_dump(Session::get('cbk_msg', null));
+
         /** @var User $user */
         $user = \Illuminate\Support\Facades\Auth::user();
 
@@ -47,5 +52,39 @@ class Course extends Controller
             }
         });
 
+        return redirect(route('student.course.start', [1]))->with('cbk_msg', ['notify' => ['Selamat Mengerjakan']]);
+    }
+
+    /**
+     * @param int $question
+     * @param Collection $answers
+     */
+    public function start($question, Collection $answers)
+    {
+        $question = intval($question);
+        $prev     = $current = $next = null;
+        /** @var Answer $answer */
+        foreach ($answers as $answer)
+        {
+            $answer_question = intval($answer->getAttribute('question'));
+            if ($answer_question === ($question - 1))
+            {
+                $prev = $answer;
+            }
+            else if ($answer_question === ($question))
+            {
+                $current = $answer;
+            }
+            else if ($answer_question === ($question + 1))
+            {
+                $next = $answer;
+            }
+        }
+        unset($answers);
+
+        $question = Question::where('id', '=', $question)->first();
+        $options  = QuestionOption::all();
+
+        dd([$prev, $current, $next, $question, $options]);
     }
 }
