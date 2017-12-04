fa187c9134673300cbf4a0b60ad6426b27f6d3a0
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-05 06:55:08 +0700

    Add Course save

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -57,9 +57,10 @@ $router->group(['namespace' => 'Student', 'prefix' => '/student'], function () u
         $router->get('/profile', ['uses' => 'Profile@edit', 'as' => 'student.profile.edit']);
         $router->patch('/profile', ['uses' => 'Profile@update', 'as' => 'student.profile.update']);
         $router->group(['middleware' => 'valid.student.profile'], function () use ($router) {
-            $router->get('/course', ['uses' => 'Course@index', 'as' => 'student.course.start']);
+            $router->get('/course', ['uses' => 'Course@index', 'as' => 'student.course.index']);
             $router->get('/course/create', ['middleware' => 'course.open.unavailable', 'uses' => 'Course@create', 'as' => 'student.course.create']);
-            $router->get('/course/start/{question}', ['middleware' => ['course.open.available', 'valid.question'], 'uses' => 'Course@start', 'as' => 'student.course.start'])->where('question', '^[1-9][0-9]*');
+            $router->get('/course/start/{question}', ['middleware' => ['course.open.available', 'valid.question'], 'uses' => 'Course@startEdit', 'as' => 'student.course.start.edit'])->where('question', '^[1-9][0-9]*');
+            $router->patch('/course/start/{question}', ['middleware' => ['course.open.available', 'valid.question'], 'uses' => 'Course@startUpdate', 'as' => 'student.course.start.update'])->where('question', '^[1-9][0-9]*');
         });
     });
 });
