de11c48877cbf94b3afc91c0977ff547e04521ed
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 03:03:48 +0700

    Add Boilerplate root

---
+++ raw/resources/views/root/root.boilerplate.blade.php
@@ -0,0 +1,29 @@
+@extends('root.root')
+
+@section('head-css-pre')
+    {{-- Normalize --}}
+    <link rel="stylesheet" href="{{asset('/assets/vendor/html5-boilerplate/dist/css/normalize.min.css')}}">
+    {{-- Main --}}
+    <link rel="stylesheet" href="{{asset('/assets/vendor/html5-boilerplate/dist/css/main.min.css')}}">
+    {{----}}
+@endsection
+
+@section('head-js-post')
+    {{--Modernizr--}}
+    <script type="text/javascript" src="{{asset('/assets/vendor/html5-boilerplate/dist/js/vendor/modernizr-3.5.0.min.js')}}"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="{{asset('/assets/vendor/html5shiv/dist/html5shiv.min.js'}}"></script>
+    <script type="text/javascript" src="{{asset('/assets/vendor/respond.js/dest/respond.min.js')}}"></script>
+    <![endif]-->
+@endsection
+
+@section('body-js-lower-pre')
+    {{-- Jquery --}}
+    <script type="text/javascript" src="{{asset('/assets/vendor/jquery/dist/jquery.min.js')}}"></script>
+    <script type="text/javascript">window.jQuery || document.write('<script src="{{asset('/assets/vendor/jquery/dist/jquery.min.js')}}"><\/script>')</script>
+    {{-- Plugins --}}
+    <script type="text/javascript" src="{{asset('/assets/vendor/html5-boilerplate/dist/js/plugins.min.js')}}"></script>
+@endsection
