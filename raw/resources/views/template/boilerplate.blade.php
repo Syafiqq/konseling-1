24a8396cc016a60ae537a3ec920c03f371556543
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 03:15:50 +0700

    Extends Root Boilerplate

--- raw/resources/views/template/boilerplate.blade.php
+++ raw/resources/views/template/boilerplate.blade.php
@@ -1,35 +1,13 @@
-<!doctype html>
-<!--suppress HtmlUnknownTarget -->
-<html class="no-js" lang="">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('root.root-boilerplate')
 
-    <link rel="manifest" href="/manifest.min.json">
-    <link rel="apple-touch-icon" href="/icon.png">
-    <!-- Place favicon.ico in the root directory -->
+@section('head-title')
+    <title>Boilerplate</title>
+@endsection
 
-    <link rel="stylesheet" href="/assets/vendor/html5-boilerplate/dist/css/normalize.min.css">
-    <link rel="stylesheet" href="/assets/vendor/html5-boilerplate/dist/css/main.min.css">
-</head>
-<body>
-<!--[if lte IE 9]>
-<p class="browserupgrade">You are using an
-    <strong>outdated</strong>
-                          browser. Please
-    <a href="https://browsehappy.com/">upgrade your browser</a>
-                          to improve your experience and security.
-</p>
-<![endif]-->
+@section('head-description')
+    <meta name="description" content="Boilerplate">
+@endsection
 
-<!-- Add your site or application content here -->
-<p>Hello world! This is HTML5 Boilerplate.</p>
-<script src="/assets/vendor/html5-boilerplate/dist/js/vendor/modernizr-3.5.0.min.js"></script>
-<script src="/assets/vendor/jquery/dist/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="/assets/vendor/jquery/dist/jquery.min.js"><\/script>')</script>
-<script src="/assets/vendor/html5-boilerplate/dist/js/plugins.min.js"></script>
-</body>
-</html>
+@section('body-content')
+    <p>Hello world! This is HTML5 Boilerplate.</p>
+@endsection
