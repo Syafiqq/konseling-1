d12bbd45b67e00c42a6ff41cff11d6a76dafeb02
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 03:15:34 +0700

    Update boilerplate

--- raw/resources/views/root/root-boilerplate.blade.php
+++ raw/resources/views/root/root-boilerplate.blade.php
@@ -1,8 +1,11 @@
 @extends('root.root')
 
 @section('head-property')
+    {{-- Tell the browser to be responsive to screen width --}}
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    {{-- Favicon--}}
     <link rel="manifest" href="{{asset('/manifest.min.json')}}">
-    @parent
+    <link rel="apple-touch-icon" href="{{asset('/icon.png')}}">
 @endsection
 
 @section('head-css-pre')
@@ -20,8 +23,19 @@
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script type="text/javascript" src="{{asset('/assets/vendor/html5shiv/dist/html5shiv.min.js'}}"></script>
+    <script type="text/javascript" src="{{asset('/assets/vendor/html5shiv/dist/html5shiv.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('/assets/vendor/respond.js/dest/respond.min.js')}}"></script>
+    <![endif]-->
+    @endsection
+
+@section('body-upgrade-browser')
+    <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an
+        <strong>outdated</strong>
+                              browser. Please
+        <a href="https://browsehappy.com/">upgrade your browser</a>
+                              to improve your experience and security.
+    </p>
     <![endif]-->
 @endsection
 
