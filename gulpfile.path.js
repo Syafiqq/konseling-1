f4c684f0023775e674515a20635343e3db39700e
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 03:04:00 +0700

    Add html5shiv and respond

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -1,6 +1,5 @@
 module.exports = {
-    assetsVendorResource: function(negated, mime)
-    {
+    assetsVendorResource: function (negated, mime) {
         return [
             (negated ? '!' : '') + './node_modules/jquery/dist/**' + (mime === null ? '' : '/' + mime),
             (negated ? '!' : '') + './node_modules/html5-boilerplate/dist/css/**' + (mime === null ? '' : '/' + mime),
@@ -13,7 +12,9 @@ module.exports = {
             (negated ? '!' : '') + './node_modules/nprogress/**' + (mime === null ? '' : '/' + mime),
             (negated ? '!' : '') + './node_modules/ionicons/dist/**' + (mime === null ? '' : '/' + mime),
             (negated ? '!' : '') + './node_modules/fastclick/lib/**' + (mime === null ? '' : '/' + mime),
-            (negated ? '!' : '') + './node_modules/admin-lte/dist/**' + (mime === null ? '' : '/' + mime)
+            (negated ? '!' : '') + './node_modules/admin-lte/dist/**' + (mime === null ? '' : '/' + mime),
+            (negated ? '!' : '') + './node_modules/html5shiv/dist/**' + (mime === null ? '' : '/' + mime),
+            (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (mime === null ? '' : '/' + mime)
         ];
     }
 };
