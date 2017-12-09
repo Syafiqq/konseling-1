06285f634f9d1b70ccc2a5765815ef3a96f82be8
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 07:53:38 +0700

    Add avatar

--- raw/resources/views/layout/counselor/extension/sidebar/theme_1_sidebar.blade.php
+++ raw/resources/views/layout/counselor/extension/sidebar/theme_1_sidebar.blade.php
@@ -1,8 +1,19 @@
+<?php
+$user = \Illuminate\Support\Facades\Auth::user();
+?>
 <!-- Left side column. contains the sidebar -->
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
         <!-- sidebar menu: : style can be found in sidebar.less -->
+        <a href="{{route('counselor.profile.edit')}}" class="dropdown-toggle" data-toggle="dropdown">
+            <img src="{{asset($user->getAttribute('avatar'))}}" class="user-image img-thumbnail img-circle center-block" width="64px" alt="User Image">
+        </a>
+        <ul class="dropdown-menu">
+            <li class="user-header">
+                <img src="{{asset($user->getAttribute('avatar'))}}" class="img-circle" alt="User Image">
+            </li>
+        </ul>
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">Menu</li>
             <li>
