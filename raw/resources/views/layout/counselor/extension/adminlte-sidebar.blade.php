2f063718d8b66f230c05aac02d2733139f8832db
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 14:37:46 +0700

    Add music player

--- raw/resources/views/layout/counselor/extension/adminlte-sidebar.blade.php
+++ raw/resources/views/layout/counselor/extension/adminlte-sidebar.blade.php
@@ -12,6 +12,31 @@ if (isset($errors))
 
 @section('body-content')
     @parent
+    <div class="modal modal-success fade" id="music-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Pemutar Musik</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            @include('shard.music-player.music-player-theme_1')
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 @endsection
 
 @section('body-content-navbar')
@@ -25,6 +50,7 @@ if (isset($errors))
     @parent
     {{-- Font Awesome --}}
     <link rel="stylesheet" href="{{asset('/assets/css/common/common-style.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/assets/css/shard/music-player/theme_1.min.css')}}">
 @endsection
 
 @section('body-js-lower-post')
@@ -33,6 +59,7 @@ if (isset($errors))
         {!! 'var sessionFlashdata = ' . json_encode(\Illuminate\Support\Facades\Session::get('cbk_msg'))!!}
     </script>
     <script type="text/javascript" src="{{asset('/assets/js/common/common_function.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/assets/js/shard/music-player/theme_1.min.js')}}"></script>
 @endsection
 
 @section('body-property')
