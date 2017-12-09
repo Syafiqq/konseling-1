eb84c965f39432c9a665384b78c540373e3ee9dd
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 14:32:20 +0700

    Add music player theme_1

---
+++ raw/resources/views/shard/music-player/music-player-theme_1.blade.php
@@ -0,0 +1,25 @@
+<div id="cover">
+    <div class="panel panel-default">
+        <audio id="audio" autoplay preload="auto" tabindex="0" controls>
+            <source src="{{asset('/assets/audio/mp3/music1.mp3')}}">
+        </audio>
+    </div>
+
+    <ul id="playlist">
+        <li class="active list-group-item">
+            <a href="{{asset('/assets/audio/mp3/music1.mp3')}}">
+                Musik 1
+            </a>
+        </li>
+        <li class="list-group-item">
+            <a href="{{asset('/assets/audio/mp3/music2.mp3')}}">
+                Musik 2
+            </a>
+        </li>
+        <li class="list-group-item">
+            <a href="{{asset('/assets/audio/mp3/music3.mp3')}}">
+                Musik 3
+            </a>
+        </li>
+    </ul>
+</div>
