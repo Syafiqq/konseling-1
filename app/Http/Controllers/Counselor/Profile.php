84b6393464f0e81832054f0fb0e9ac2d4957d7ec
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 07:23:27 +0700

    Remove edit

--- app/Http/Controllers/Counselor/Profile.php
+++ app/Http/Controllers/Counselor/Profile.php
@@ -5,7 +5,6 @@ use App\Eloquent\UserCounselors;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Session;
 
 class Profile extends Controller
 {
@@ -33,9 +32,6 @@ class Profile extends Controller
      */
     public function edit()
     {
-        /** @noinspection PhpUndefinedMethodInspection */
-        var_dump(Session::get('cbk_msg', null));
-
         return view("layout.counselor.profile.edit.counselor_profile_edit_$this->theme", ['counselor' => $this->counselor]);
     }
 
