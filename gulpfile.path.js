0b5ebccc3d023069dcd135116a279b94f3fcf46f
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-09 07:02:15 +0700

    Add datatable

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -15,7 +15,10 @@ module.exports = {
             (negated ? '!' : '') + './node_modules/admin-lte/dist/**' + (mime === null ? '' : '/' + mime),
             (negated ? '!' : '') + './node_modules/html5shiv/dist/**' + (mime === null ? '' : '/' + mime),
             (negated ? '!' : '') + './node_modules/respond.js/dest/**' + (mime === null ? '' : '/' + mime),
-            (negated ? '!' : '') + './node_modules/bootstrap-notify/**' + (mime === null ? '' : '/' + mime)
+            (negated ? '!' : '') + './node_modules/bootstrap-notify/**' + (mime === null ? '' : '/' + mime),
+            (negated ? '!' : '') + './node_modules/datatables.net/**' + (mime === null ? '' : '/' + mime),
+            (negated ? '!' : '') + './node_modules/datatables.net-dt/**' + (mime === null ? '' : '/' + mime),
+            (negated ? '!' : '') + './node_modules/datatables.net-bs/**' + (mime === null ? '' : '/' + mime)
         ];
     }
 };
