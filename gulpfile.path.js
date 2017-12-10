09871df453efc6f2003327cdf005fa90b49da523
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-10 09:49:25 +0700

    Add icheck

--- gulpfile.path.js
+++ gulpfile.path.js
@@ -20,6 +20,7 @@ module.exports = {
             (negated ? '!' : '') + './node_modules/datatables.net-dt/**' + (mime === null ? '' : '/' + mime),
             (negated ? '!' : '') + './node_modules/datatables.net-bs/**' + (mime === null ? '' : '/' + mime),
             (negated ? '!' : '') + './node_modules/jQuery.print/**' + (mime === null ? '' : '/' + mime),
+            (negated ? '!' : '') + './/node_modules/icheck/**' + (mime === null ? '' : '/' + mime)
         ];
     }
 };
