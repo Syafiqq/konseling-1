301beb308a2e1b71835ab84aa6debea96e5f7ff2
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-10 20:53:07 +0700

    Change parent to boilerplate Change all resource to internal baked resources

--- raw/resources/views/layout/bootstrap-authentication.blade.php
+++ raw/resources/views/layout/bootstrap-authentication.blade.php
@@ -1,4 +1,4 @@
-@extends('root.root-bootstrap')
+@extends('root.root-boilerplate')
 <?php
 /** @var \Collective\Html\FormBuilder $form */
 setlocale(LC_TIME, 'Indonesian');
@@ -17,12 +17,9 @@ if (!is_null($session->get('cbk_msg')))
 ?>
 
 @section('head-css-pre')
-    @parent
-    <link rel="stylesheet" href="{{asset('/assets/vendor/font-awesome/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/assets/vendor/ionicons/dist/css/ionicons.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/assets/vendor/nprogress/nprogress.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/assets/baked/authentication/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('/assets/baked/authentication/css/style.min.css')}}">
-    <link rel="stylesheet" href="{{asset('/assets/vendor/icheck/skins/square/blue.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/assets/baked/authentication/css/font-awesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('/assets/css/common/common-style.min.css')}}">
     <link rel="stylesheet" href="{{asset('/assets/css/shard/music-player/theme_1.min.css')}}">
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=EB+Garamond">
@@ -42,12 +39,10 @@ if (!is_null($session->get('cbk_msg')))
 
 @section('body-js-lower-pre')
     @parent
-    <script type="text/javascript" src="{{asset('/assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('/assets/vendor/fastclick/lib/fastclick.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('/assets/vendor/nprogress/nprogress.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/assets/baked/authentication/js/jquery.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('/assets/baked/authentication/js/bootstrap.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('/assets/vendor/bootstrap-notify/bootstrap-notify.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('/assets/baked/authentication/js/jquery.backstretch.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('/assets/vendor/icheck/icheck.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('/assets/baked/authentication/js/default-backstretch.min.js')}}"></script>
     <script type="text/javascript">
         {!! 'var sessionFlashdata = ' . json_encode($flashdata)!!}
