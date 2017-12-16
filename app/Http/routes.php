512d5d7d7c06f774ffbc13187778e0ae4f4c595e
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-17 05:07:08 +0700

    Register recover route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,6 +26,7 @@ $router->group(['namespace' => 'Counselor', 'prefix' => '/counselor'], function 
             $router->post('/login', ['middleware' => 'auth.role', 'uses' => 'Auth@postLogin', 'as' => 'counselor.auth.login.post']);
             $router->get('/lost', ['uses' => 'Auth@getLost', 'as' => 'counselor.auth.lost.get']);
             $router->post('/lost', ['middleware' => 'auth.role', 'uses' => 'Auth@postLost', 'as' => 'counselor.auth.lost.post']);
+            $router->get('/recover', ['middleware' => ['auth.role', 'valid.auth.recovery'], 'uses' => 'Auth@getRecover', 'as' => 'counselor.auth.recover.get']);
         });
     });
     $router->group(['middleware' => 'authenticated.source'], function () use ($router) {
