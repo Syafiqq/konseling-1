972b41720e20a86fa41c1fddabdf8e8791bef97f
Syafiq <syafiq.rezpector@gmail.com>
2018-11-28 23:29:39 +0700

    Narrow down instrument music

--- raw/resources/views/shard/music-player/music-player-theme_1.blade.php
+++ raw/resources/views/shard/music-player/music-player-theme_1.blade.php
@@ -1,15 +1,6 @@
 <?php
 $musics = [
     ['name' => 'Instrument 1', 'source' => '/assets/audio/mp3/InstrumenAplas_1.mp3'],
-    ['name' => 'Instrument 2', 'source' => '/assets/audio/mp3/InstrumenAplas_2.mp3'],
-    ['name' => 'Instrument 3', 'source' => '/assets/audio/mp3/InstrumenAplas_3.mp3'],
-    ['name' => 'Instrument 4', 'source' => '/assets/audio/mp3/InstrumenAplas_4.mp3'],
-    ['name' => 'Instrument 5', 'source' => '/assets/audio/mp3/InstrumenAplas_5.mp3'],
-    ['name' => 'Instrument 6', 'source' => '/assets/audio/mp3/InstrumenAplas_6.mp3'],
-    ['name' => 'Instrument 7', 'source' => '/assets/audio/mp3/InstrumenAplas_7.mp3'],
-    ['name' => 'Instrument 8', 'source' => '/assets/audio/mp3/InstrumenAplas_8.mp3'],
-    ['name' => 'Instrument 9', 'source' => '/assets/audio/mp3/InstrumenAplas_9.mp3'],
-    ['name' => 'Instrument 10', 'source' => '/assets/audio/mp3/InstrumenAplas_10.mp3']
 ]
 ?>
 <div id="cover">
